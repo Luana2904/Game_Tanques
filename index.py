2c422f09c7358a581ac142fbf20481e42516786a
Luana Rodrigues <marialuana2810@gmail.com>
2022-10-08 10:55:57 -0300

    Instrucoes basicas sobre o jogo

---
+++ index.py
@@ -0,0 +1,20 @@
+'''
+O jogo Gorilas é um jogo clássico de turnos, no qual cada jogador espera sua vez de jogar.
+
+Os links em anexo mostram exemplos de implementações e permitem entender o que é esperado na entrega.
+
+O jogo deve ter:
+1 - Menu de entrada;
+2 - Ranking dos melhores jogadores;
+3 - Configurações de nível de dificuldade.
+
+Funcionalidades para bolinha:
+- Entradas: Ângulo, velocidade e gravidade(Não sei se colocamos para o usuario escolher ou deixamos em 9.8)
+- Caso a bolinha bata no gorila apenas uma vez, o jogador que jogou ganha
+- Caso a bolinha bata no prédio (ou passarinho) ele explode
+
+As configurações podem ser feitas quando usuário iniciar o jogo ou disponibilizando uma opção no menu de entrada. Cada grupo decide como fazer.
+
+como instalar o curses: pip install windows-curses
+'''
+
