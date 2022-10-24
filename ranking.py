893198e6a62643d01ef7db56444a7cae05dbc56d
Luana Rodrigues <marialuana2810@gmail.com>
2022-10-24 14:44:15 -0300

    .

--- ranking.py
+++ ranking.py
@@ -15,22 +15,6 @@ with open("informacao.json") as fp:
 with open("informacao.json", 'w') as json_file:
     json.dump(listobj, json_file, indent= 4) #linha onde o json se altera
 
-<<<<<<< HEAD
-with open("informacao.json", 'r'):
-    for i in listobj:
-        if listobj[i]['pontuacao'] > maior:
-            maior = listobj[i]['nome']
-            index = i
 
-            
-=======
-#with open("informacao.json", 'r'):
-    #for i in listobj:
-        #if listobj[i]['pontuacao'] > maior:
-            #maior = listobj[i]['nome']
-            #index = i
-        
-
->>>>>>> 7a3cfa75db113694cb5c72304261eb0990ac1950
 
         
