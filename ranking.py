0e9d2510bd315c83285308a6346e090d28b48d8b
VRAPGI <viniciusfr2003@gmail.com>
2022-10-24 14:34:28 -0300

    nao vai dar merge

--- ranking.py
+++ ranking.py
@@ -2,24 +2,25 @@ import curses
 import menu
 import json
 
+
 listobj= []
 
 with open("informacao.json") as fp:
     listobj= json.load(fp)
     listobj.append({
-        "nome": "claudio", #usuario, usuario2 //  loop do apend: lista JOGADOR, i in range o len(JOGADOR)
-        "pontuacao": 36 #criar lista p pontuação? 
+        "nome": "claudia", #usuario, usuario2 //  loop do apend: lista JOGADOR, i in range o len(JOGADOR)
+        "pontuacao": 38 #criar lista p pontuação? 
     })
 
-with open("informacao.json", 'r+') as json_file:
+with open("informacao.json", 'w') as json_file:
     json.dump(listobj, json_file, indent= 4) #linha onde o json se altera
 
-with open("informacao.json", 'r'):
-    for i in listobj:
-        if listobj[i]['pontuacao'] > maior:
-            maior = listobj[i]['nome']
-            index = i
-        else:
-            
+#with open("informacao.json", 'r'):
+    #for i in listobj:
+        #if listobj[i]['pontuacao'] > maior:
+            #maior = listobj[i]['nome']
+            #index = i
+        
+
 
         
